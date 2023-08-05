337174012ff613b2a8cdc9708f2696ff304d9e80
Sajid Khan Afridi <uetianafridi@gmail.com>
2023-08-05 15:05:54 +0500

    a

--- task2_filter_component/src/components/Filter.tsx
+++ task2_filter_component/src/components/Filter.tsx
@@ -3,16 +3,17 @@
 import React, { useState } from "react";
 import Wrapper from "@/components/Wrapper";
 import Circle from "@/components/Circle";
-import SubComponent from "@/components/SubComponent";
+import SubComponent2 from "@/components/SubComponent2";
+import SubComponent3 from "./subComponent3";
 
 const Filter = () => {
   return (
     <Wrapper>
-      <SubComponent
+      {/* <SubComponent2
         heading="Availability"
         subHeading={["In stock", "Out of stock"]}
-      />
-      <SubComponent heading="Size" subHeading={["COMPACT", "GRANDE", "MIDI"]} />
+      /> */}
+      {/* <SubComponent heading="Size" subHeading={["COMPACT", "GRANDE", "MIDI"]} />
       <SubComponent heading="Product type" subHeading={["Tote Bag"]} />
       <SubComponent
         heading="Sort by"
@@ -25,7 +26,8 @@ const Filter = () => {
           "Date, old to new",
           "Date, new to old",
         ]}
-      />
+      /> */}
+      <SubComponent3 />
     </Wrapper>
   );
 };
---
+++ task2_filter_component/src/components/SubComponent2.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import Circle from "@/components/Circle";
+
+interface MyProps {
+  heading: string;
+  subHeading: string[];
+}
+
+const SubComponent2 = (props: MyProps) => {
+  const [show, setShow] = useState(false);
+  function handleClick() {
+    setShow(!show);
+  }
+  const divStyles = {
+    cursor: "pointer",
+    fontSize: "2rem",
+    transform: show ? "rotate(0deg)" : "rotate(180deg)",
+    transition: "all 0.3s ease",
+  };
+  const dropdownStyles = {
+    cursor: "pointer",
+    opacity: 1,
+    transform: show ? "translateY(0)" : "",
+    transition: "0.5s ease-out",
+    transitionProperty: "transform",
+  };
+  return (
+    <div className="flex flex-col">
+      <div className="flex justify-between">
+        <div className="mt-3">{props.heading}</div>
+        <div
+          onClick={handleClick}
+          className="cursor-pointer text-3xl focus:rotate-180 delay-300 transition-all ease-in-out duration-500"
+          style={divStyles}
+        >
+          {show ? "-" : "+"}
+        </div>
+      </div>
+
+      <div
+        className={`${
+          show
+            ? "flex flex-col ml-2 mt-1 transition-all delay-1000 duration-700" //drop content
+            : "hidden"
+        } `}
+        style={dropdownStyles}
+      >
+        {props.subHeading.map((item) => (
+          <div className="flex mb-2" key={item}>
+            <Circle /> <span className="pl-3">{item}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SubComponent2;
