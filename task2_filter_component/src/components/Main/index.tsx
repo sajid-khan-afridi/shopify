19111427d125d8243b2c6642dee4024474e5b2cc
Sajid Khan Afridi <uetianafridi@gmail.com>
2023-08-09 22:44:08 +0500

    changes

--- task2_filter_component/src/components/Main/index.tsx
+++ task2_filter_component/src/components/Main/index.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import Filter from "../Filter";
 import CollectionGrid from "../hira";
+import "./app.css";
 
 const Main = () => {
   return (
     <div className="min-h-screen grid grid-cols-[1fr,3fr] gap-5 mt-10">
-      <div className="p-5 bg-gray-300 ">
+      <div className="p-5 max-h-[800px] max-md:hidden">
         <Filter />
       </div>
-      <div className="p-5 bg-gray-300 ">
+      <div
+        className="p-5 max-h-[800px] overflow-scroll no-scrollbar max-md:w-screen"
+        style={{ scrollbarWidth: "none" }}
+      >
         <CollectionGrid />
       </div>
     </div>
